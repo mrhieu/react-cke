d4508e7a24304685f62e5995b32520ea29e4ddab
Pham Van Hieu <mr_hie@yahoo.com>
2019-06-06 17:18:31 +0800

    chore: add ckeditor5-react

--- src/App.js
+++ src/App.js
@@ -1,12 +1,32 @@
 import React from 'react';
-import logo from './logo.svg';
+import CKEditor from '@ckeditor/ckeditor5-react';
+import ClassicEditor from '@ckeditor/ckeditor5-build-classic';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <CKEditor
+          editor={ ClassicEditor }
+          data="<p>Hello from CKEditor 5!</p>"
+          onInit={ editor => {
+            // You can store the "editor" and use when it is needed.
+            console.log( 'Editor is ready to use!', editor );
+          } }
+          onChange={ ( event, editor ) => {
+            const data = editor.getData();
+            console.log( { event, editor, data } );
+          } }
+          onBlur={ editor => {
+            console.log( 'Blur.', editor );
+          } }
+          onFocus={ editor => {
+            console.log( 'Focus.', editor );
+          } }
+        />
+
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
