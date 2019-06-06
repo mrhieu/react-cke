d9452b44097c9ca2b55383f385e6ba35cd3e686a
Pham Van Hieu <mr_hie@yahoo.com>
2019-06-06 18:02:32 +0800

    try to use the build directly

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import CKEditor from '@ckeditor/ckeditor5-react';
-import ClassicEditor from '@ckeditor/ckeditor5-build-classic';
+// import ClassicEditor from '@ckeditor/ckeditor5-build-classic/build/ckeditor.js';
+import ClassicEditor from './ckeditor';
 
 import './App.css';
 
